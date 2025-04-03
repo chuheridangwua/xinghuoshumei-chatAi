ca67784ecc6e35dd0cf17ae1b3241407d1df712d
jiaoyiyang <jiaoyiyang@sinosoft.com.cn>
2025-04-03 13:17:42 +0800

    添加忽略文件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <script setup lang="ts">
-import axios from 'axios';
 import HelloWorld from './components/HelloWorld.vue'
-var header = {
-  'Authorization': 'Bearer app-NnQlkYp2Wb9AQ05zvQPjGcWn'
-}
-function startToDify(){
-  axios.post('http://192.168.79.122:8083/v1/chat-messages',{
-    query:"帮我查询一下金钟这个客户的信息",
-    user:"ldq",
-     "response_mode": "streaming",
-     "inputs": {},
-  },{headers:{'Authorization': 'Bearer app-NnQlkYp2Wb9AQ05zvQPjGcWn'}})
-}
+
 </script>
 
 <template>
   <div>
-    <button class="button" @click="startToDify">Dify</button>
     <div class="chatBox">
       <HelloWorld></HelloWorld>
     
@@ -25,12 +13,7 @@ function startToDify(){
 </template>
 
 <style scoped>
-  .button {
-    background-color: red;
-    color:#fff;
-    font-size:26px;
-    font-weight:600;
-  }
+
   .chatBox {
     position:absolute;
     bottom:40px;
